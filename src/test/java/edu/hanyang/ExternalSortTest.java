5b14a3a35dd7862061d1a59ae79e0007eaffa7ac
Keonwoo kim <kdbml314@gmail.com>
2018-03-28 23:16:26 +0900

    Add Test code

--- src/test/java/edu/hanyang/ExternalSortTest.java
+++ src/test/java/edu/hanyang/ExternalSortTest.java
@@ -7,76 +7,66 @@ import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
 
-import org.apache.commons.lang3.tuple.MutableTriple;
+import org.junit.Before;
 import org.junit.Test;
 
 import edu.hanyang.submit.TinySEExternalSort;
 
 public class ExternalSortTest {
+	@Before
+	public void init() {
+		clean("./tmp");
+		File resultFile = new File("./sorted.data");
+		if(resultFile.exists()) {
+			resultFile.delete();
+		}
+	}
+	
 	@Test
-	public void sampleDataTest() throws IOException {
-		TinySEExternalSort sort = new TinySEExternalSort();
+	public void TestSort() throws IOException {
 		int blocksize = 1024;
 		int nblocks = 10;
-		String infile = "./data/test-100000.data";
-		String outfile = "./result";
+		ClassLoader classLoader = getClass().getClassLoader();
+		File infile = new File(classLoader.getResource("test.data").getFile());
+		String outfile = "./sorted.data";
 		String tmpdir = "./tmp";
-		sort.sort(infile, outfile, tmpdir, blocksize, nblocks);
-
-		// In memory sort
-		DataInputStream reader = new DataInputStream(new BufferedInputStream(new FileInputStream(infile)));
-		ArrayList<MutableTriple<Integer, Integer, Integer>> arr = new ArrayList<>();
-		try {
-			while (true) {
-				arr.add(new MutableTriple<Integer, Integer, Integer>(reader.readInt(), reader.readInt(), reader.readInt()));
-			}
-		} catch (IOException e) {
-		}
-		Collections.sort(arr);
-		reader.close();
-
-		// External sort
-		File file = new File("./result");
-		File[] files = file.listFiles();
-		assertEquals(files.length, 1);
+		File resultFile = new File(outfile);
 		
-		DataInputStream reader2 = new DataInputStream(new BufferedInputStream(new FileInputStream(files[0].getAbsoluteFile())));
-		ArrayList<MutableTriple<Integer, Integer, Integer>> arr2 = new ArrayList<>();
-		try {
-			while (true) {
-				arr2.add(new MutableTriple(reader2.readInt(), reader2.readInt(), reader2.readInt()));
-			}
-		} catch (IOException e) {
-		}
-
-		// Compare
-		assertEquals(arr.size(), arr2.size());
-		for (int i = 0; i < arr2.size(); i++) {
-			assertEquals(arr.get(i), arr2.get(i));
-		}
-	}
-
-	@Test
-	public void bigdataTest() throws IOException {
 		TinySEExternalSort sort = new TinySEExternalSort();
-		int blocksize = 8192;
-		int nblocks = 10;
-		String infile = "./data/test-100000000.data";
-		String outfile = "./result";
-		String tmpdir = "./tmp";
+		sort.sort(infile.getAbsolutePath(), outfile, tmpdir, blocksize, nblocks);
+
 		
-		do {
-			try {
-				sort.sort(infile, outfile, tmpdir, blocksize, nblocks);
-			}
-			catch (OutOfMemoryError e) {
-				nblocks /= 2;
-			}
+		File answerFile = new File(classLoader.getResource("answer.data").getFile());
+		DataInputStream resultInputStream = new DataInputStream(new BufferedInputStream(new FileInputStream(resultFile)));
+		DataInputStream answerInputStream = new DataInputStream(new BufferedInputStream(new FileInputStream(answerFile)));
+
+		assertNotNull(resultInputStream);
+		assertNotNull(answerInputStream);
+
+		for (int i = 0; i < 100000; i++) {
+			assertEquals(resultInputStream.readInt(), answerInputStream.readInt());
+			assertEquals(resultInputStream.readInt(), answerInputStream.readInt());
+			assertEquals(resultInputStream.readInt(), answerInputStream.readInt());
 		}
-		while (nblocks > 0);
+
+		resultInputStream.close();
+		answerInputStream.close();
 	}
 
+	private void clean(String dir) {
+		File file = new File(dir);
+		File[] tmpFiles = file.listFiles();
+		if (tmpFiles != null) {
+			for (int i = 0; i < tmpFiles.length; i++) {
+				if (tmpFiles[i].isFile()) {
+					tmpFiles[i].delete();
+				} else {
+					clean(tmpFiles[i].getAbsolutePath());
+				}
+				tmpFiles[i].delete();
+			}
+			file.delete();
+		}
+	}
 }
