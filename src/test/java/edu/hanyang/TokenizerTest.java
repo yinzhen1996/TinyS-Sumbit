d7d6ce4f98af20f070186541873bd13bb2d9b84b
Younghoon Kim <nongaussian@gmail.com>
2018-03-28 10:00:10 +0900

    first commit

---
+++ src/test/java/edu/hanyang/TokenizerTest.java
@@ -0,0 +1,69 @@
+package edu.hanyang;
+ 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+ 
+import java.util.ArrayList;
+import java.util.List;
+ 
+import org.junit.BeforeClass;
+import org.junit.Test;
+ 
+import edu.hanyang.submit.TinySETokenizer;
+ 
+public class TokenizerTest {
+	static List<String[]> results;
+	static List<String> testSentences;
+ 
+	@BeforeClass
+	public static void init() {
+		results = new ArrayList<String[]>();
+		
+		String[] result1 = { "unless", "she", "i", "more", "earnest", "in", "her", "studi", "she", "will", "not", "be", "abl", "to", "keep", "up", "with",
+				"the", "class" };
+		String[] result2 = { "where", "will", "the", "next", "olymp", "be", "held" };
+		String[] result3 = { "i", "read", "thi", "book", "the", "other", "dai", "and", "found", "it", "veri", "interest", "so", "i", "will", "give", "it", "to",
+				"you" };
+		String[] result4 = { "the", "test", "email", "mail", "javacodegeek", "com" };
+		String[] result5 = { "won", "t", "you", "go", "to", "that", "island", "with", "me", "if", "it", "i", "fine", "tomorrow", "afternoon", "it", "take",
+				"about", "an", "hour", "by", "boat" };
+ 
+		results.add(result1);
+		results.add(result2);
+		results.add(result3);
+		results.add(result4);
+		results.add(result5);
+ 
+		testSentences = new ArrayList<String>();
+		testSentences.add("Unless she is more earnest in her studies, she will not be able to keep up with the class.");
+		testSentences.add("Where will the next Olympics be held?");
+		testSentences.add("I read this book the other day and found it very interesting, so I will give it to you.");
+		testSentences.add("The test email - mail@javacodegeeks.com");
+		testSentences.add("Won't you go to that island with me if it is fine tomorrow afternoon? It takes about an hour by boat.");
+	
+	}
+ 
+	@Test
+	public void TestTokenizer() {
+		try {
+			// external code binding
+			TinySETokenizer obj = new TinySETokenizer();
+			assertNotNull(obj);
+			
+			obj.setup();
+			for (int i = 0; i < testSentences.size(); i++) {
+				List<String> arr = obj.split(testSentences.get(i));
+				assertNotNull(arr);
+ 
+				assertEquals(arr.size(), results.get(i).length);
+				for (int cnt = 0; cnt < arr.size(); cnt++) {
+					assertTrue(results.get(i)[cnt].equals(arr.get(cnt)));
+				}
+			}
+			obj.clean();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ target/classes/edu/hanyang/submit/TinySETokenizer.java
@@ -0,0 +1,19 @@
+package edu.hanyang.submit;
+
+import java.util.List;
+
+import edu.hanyang.indexer.Tokenizer;
+
+public class TinySETokenizer implements Tokenizer {
+
+	public void setup() {
+	}
+
+	public List<String> split(String text) {
+		return null;
+	}
+
+	public void clean() {
+	}
+
+}
