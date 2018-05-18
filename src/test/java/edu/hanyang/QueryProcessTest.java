016d7542e0e0e806e8bf89c15ce4f632bc1671ce
Younghoon Kim <nongaussian@gmail.com>
2018-05-18 17:13:45 +0900

    minor

---
+++ src/test/java/edu/hanyang/QueryProcessTest.java
@@ -0,0 +1,18 @@
+package edu.hanyang;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class QueryProcessTest {
+	@Before
+	public void init () {
+		
+	}
+	
+	@Test
+	public void test_query1() throws IOException {
+		
+	}
+}
