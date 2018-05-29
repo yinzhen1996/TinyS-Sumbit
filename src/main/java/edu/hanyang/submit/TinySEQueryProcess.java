7c3dd9415f2a41d07c49e23991b6f0ff663e5d65
Younghoon Kim <nongaussian@gmail.com>
2018-05-29 09:11:51 +0900

    template code

--- src/main/java/edu/hanyang/submit/TinySEQueryProcess.java
+++ src/main/java/edu/hanyang/submit/TinySEQueryProcess.java
@@ -3,17 +3,19 @@ package edu.hanyang.submit;
 import java.io.IOException;
 
 import edu.hanyang.indexer.DocumentCursor;
+import edu.hanyang.indexer.PositionCursor;
 import edu.hanyang.indexer.IntermediateList;
+import edu.hanyang.indexer.IntermediatePositionalList;
 import edu.hanyang.indexer.QueryPlanTree;
 
 public class TinySEQueryProcess {
-	public void op_and_wo_pos (DocumentCursor op1, DocumentCursor op2, IntermediateList out) {
+	public void op_and_wo_pos (DocumentCursor op1, DocumentCursor op2, IntermediateList out) throws IOException {
 	
 	}
-	public void op_and_w_pos (DocumentCursor op1, DocumentCursor op2, int shift, IntermediateList out) {
+	public void op_and_w_pos (DocumentCursor op1, DocumentCursor op2, int shift, IntermediatePositionalList out) throws IOException {
 		
 	}
-	public QueryPlanTree parse_query(String query){
+	public QueryPlanTree parse_query(String query) throws IOException {
 		return null;
 	}
 }
